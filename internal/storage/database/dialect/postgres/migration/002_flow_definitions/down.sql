20dec5dff9dadd3018b4039223d258a1c7f80c6e
Marco Ardizzone <marco@zitadel.com>
2026-04-27 17:12:58 +0200

    Add enums for states and purposes

--- internal/storage/database/dialect/postgres/migration/002_flow_definitions/down.sql
+++ internal/storage/database/dialect/postgres/migration/002_flow_definitions/down.sql
@@ -1,6 +1,3 @@
-DROP TABLE IF EXISTS zitadel_nextgen.flow_definition_step_transitions;
-DROP TABLE IF EXISTS zitadel_nextgen.flow_definition_steps;
-DROP TABLE IF EXISTS zitadel_nextgen.flow_definition_audiences;
-DROP TABLE IF EXISTS zitadel_nextgen.flow_definition_purposes;
+DROP INDEX IF EXISTS zitadel_nextgen.idx_flow_definitions_instance_purpose;
 DROP INDEX IF EXISTS zitadel_nextgen.idx_flow_definitions_instance_status;
 DROP TABLE IF EXISTS zitadel_nextgen.flow_definitions;
