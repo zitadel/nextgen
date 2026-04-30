433c76725c0712ce5512147bda21ff85b430afcf
Vitor Bari Buccianti <vitor+github@zitadel.com>
2026-04-30 06:46:25 +0200

    refactor: embed UnimplementedHandler in API handler (#44)
    Embed `api.UnimplementedHandler` in the `Handler` struct so that new API
    endpoints get default "not implemented" responses automatically.
    
    This removes the need to manually add handler stubs every time the API
    contract changes (see
    [adfb8af](https://github.com/zitadel/nextgen/pull/41/commits/adfb8aff6583e2b5467b39b791f8ee7442b6e1a2)).

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -2,12 +2,14 @@ package api
 
 import (
 	"context"
-	"fmt"
 
 	api "github.com/zitadel/nextgen/api/generated"
 )
 
 type Handler struct {
+	// UnimplementedHandler is embedded to provide default "not implemented"
+	// responses for all endpoints, so only implemented methods need to be defined.
+	api.UnimplementedHandler
 }
 
 func NewHandler() *Handler {
@@ -22,22 +24,6 @@ func (h Handler) NewError(ctx context.Context, err error) *api.ErrorDetailsStatu
 			Message: err.Error(),
 		},
 	}
-}
-
-func (h Handler) CreateFlow(ctx context.Context, req *api.CreateFlowRequest) (api.CreateFlowRes, error) {
-	return nil, fmt.Errorf("not implemented")
-}
-
-func (h Handler) GetFlowStep(ctx context.Context, params api.GetFlowStepParams) (api.GetFlowStepRes, error) {
-	return nil, fmt.Errorf("not implemented")
-}
-
-func (h Handler) SubmitFlowStep(ctx context.Context, req *api.FlowSubmitRequest, params api.SubmitFlowStepParams) (api.SubmitFlowStepRes, error) {
-	return nil, fmt.Errorf("not implemented")
-}
-
-func (h Handler) SubmitFlowEvent(ctx context.Context, req *api.FlowEventRequest, params api.SubmitFlowEventParams) error {
-	return fmt.Errorf("not implemented")
 }
 
 var _ api.Handler = (*Handler)(nil)
