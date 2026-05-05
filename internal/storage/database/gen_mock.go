7361b551021759083888f7c8df250c52694f5a02
Wim Van Laer <wim+github@zitadel.com>
2026-05-05 08:48:28 +0200

    Update mockgen commands to use 'go tool'

--- internal/storage/database/gen_mock.go
+++ internal/storage/database/gen_mock.go
@@ -1,5 +1,5 @@
 package database
 
 //go:generate mkdir -p ./dbmock
-//go:generate mockgen -source=./database.go -aux_files github.com/zitadel/nextgen/internal/storage/database=./tx.go,github.com/zitadel/nextgen/internal/storage/database=./migration.go -package=dbmock -destination=./dbmock/database.mock.go
-//go:generate mockgen -source=./tx.go -aux_files github.com/zitadel/nextgen/internal/storage/database=./database.go -package=dbmock -destination=./dbmock/tx.mock.go
+//go:generate go tool mockgen -source=./database.go -aux_files github.com/zitadel/nextgen/internal/storage/database=./tx.go,github.com/zitadel/nextgen/internal/storage/database=./migration.go -package=dbmock -destination=./dbmock/database.mock.go
+//go:generate go tool mockgen -source=./tx.go -aux_files github.com/zitadel/nextgen/internal/storage/database=./database.go -package=dbmock -destination=./dbmock/tx.mock.go
