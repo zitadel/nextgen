c6afd78019dccc32583d308f488fab5235aa2366
Tim Möhlmann <tim+github@zitadel.com>
2026-05-18 19:28:44 +0000

    fix: regenerate api stubs

--- api/generated/oas_validators_gen.go
+++ api/generated/oas_validators_gen.go
@@ -2614,6 +2614,29 @@ func (s *SessionWithTokenResponse) Validate() error {
 	return nil
 }
 
+func (s *SessionWithTokenResponseHeaders) Validate() error {
+	if s == nil {
+		return validate.ErrNilPointer
+	}
+
+	var failures []validate.FieldError
+	if err := func() error {
+		if err := s.Response.Validate(); err != nil {
+			return err
+		}
+		return nil
+	}(); err != nil {
+		failures = append(failures, validate.FieldError{
+			Name:  "Response",
+			Error: err,
+		})
+	}
+	if len(failures) > 0 {
+		return &validate.Error{Fields: failures}
+	}
+	return nil
+}
+
 func (s *SubmitFlowStepBadRequest) Validate() error {
 	alias := (*FlowResponseHeaders)(s)
 	if err := alias.Validate(); err != nil {
