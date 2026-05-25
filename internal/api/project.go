3087124e28de793c8de57cf614c4dd51e0ccef81
Marco Ardizzone <marco@zitadel.com>
2026-05-25 16:06:24 +0200

    Better error handling

--- internal/api/project.go
+++ internal/api/project.go
@@ -11,13 +11,7 @@ import (
 func (h Handler) CreateProject(ctx context.Context, req *api.CreateProjectRequest) (api.CreateProjectRes, error) {
 	project, err := h.projectService.Create(ctx, req.PreviewOrigins)
 	if err != nil {
-		return &api.ErrorDetailsStatusCode{
-			StatusCode: 500,
-			Response: api.ErrorDetails{
-				Code:    "internal_error",
-				Message: err.Error(),
-			},
-		}, nil
+		return h.NewError(ctx, err), nil
 	}
 	return &api.CreateProjectResponse{
 		ID:             project.ID,
@@ -37,13 +31,7 @@ func (h Handler) GetProject(ctx context.Context, params api.GetProjectParams) (a
 				Message: "project not found: " + notFound.Error(),
 			}, nil
 		}
-		return &api.ErrorDetailsStatusCode{
-			StatusCode: 500,
-			Response: api.ErrorDetails{
-				Code:    "internal_error",
-				Message: err.Error(),
-			},
-		}, nil
+		return h.NewError(ctx, err), nil
 	}
 	return &api.GetProjectResponse{
 		ID:        project.ID,
