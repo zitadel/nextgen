42cf6b345d73ca037459addd6c9cd507ce18e57c
Wim Van Laer <wim+github@zitadel.com>
2026-06-11 09:06:56 +0200

    added otel

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -24,13 +24,12 @@ import (
 	"github.com/zitadel/nextgen/internal/api"
 	"github.com/zitadel/nextgen/internal/api/middleware"
 	"github.com/zitadel/nextgen/internal/bootstrap/users"
-	"github.com/zitadel/nextgen/internal/build"
 	"github.com/zitadel/nextgen/internal/crypto"
 	"github.com/zitadel/nextgen/internal/domain"
 	"github.com/zitadel/nextgen/internal/domain/idgen"
 	"github.com/zitadel/nextgen/internal/instrumentation"
-	"github.com/zitadel/nextgen/internal/instrumentation/otel"
 	"github.com/zitadel/nextgen/internal/instrumentation/zlog"
+	"github.com/zitadel/nextgen/internal/instrumentation/zotel"
 	"github.com/zitadel/nextgen/internal/secrets"
 	"github.com/zitadel/nextgen/internal/service"
 	"github.com/zitadel/nextgen/internal/staticui/console"
@@ -41,9 +40,9 @@ import (
 	"github.com/zitadel/nextgen/internal/storage/database/repository"
 	"github.com/zitadel/oidc/v3/pkg/op"
 	"go.opentelemetry.io/contrib/bridges/otelslog"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/sdk/resource"
-	semconv "go.opentelemetry.io/otel/semconv/v1.39.0"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/log"
+	"go.opentelemetry.io/otel/propagation"
 )
 
 func NewCommand() *cobra.Command {
@@ -215,7 +214,10 @@ func run(ctx context.Context, cfg Config, userFiles []string) error {
 		api.NewSecurityHandler(),
 		oasapi.WithMiddleware(
 			middleware.AddOperationIdToContext(),
+			// logging is done at net/http level
 		),
+		oasapi.WithMeterProvider(otel.GetMeterProvider()),
+		oasapi.WithTracerProvider(otel.GetTracerProvider()),
 		oasapi.WithErrorHandler(api.OgenErrorHandler))
 	if err != nil {
 		return fmt.Errorf("failed to build api server: %w", err)
@@ -444,15 +446,41 @@ func buildCrypter(hexKey string) (crypto.Crypter, error) {
 
 func SetUpInstrumentation(
 	ctx context.Context,
-	sf *ShutdownFuncs,
+	sfs *ShutdownFuncs,
 	cfg instrumentation.Config,
 ) error {
-	otelResource, err := createResource(cfg.ServiceName)
+	otelResource, err := zotel.CreateResource(cfg.ServiceName)
 	if err != nil {
 		return err
 	}
 
-	err = setUpLogging(ctx, sf, cfg.Log, otelResource)
+	propagator := propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	)
+	otel.SetTextMapPropagator(propagator)
+
+	tracerProvider, err := zotel.NewTracerProvider(ctx, cfg.Trace.Exporter, cfg.Trace.Fraction, otelResource)
+	if err != nil {
+		return nil
+	}
+	sfs.Add(tracerProvider.Shutdown)
+	otel.SetTracerProvider(tracerProvider)
+
+	meterProvider, err := zotel.NewMeterProvider(ctx, cfg.Metric.Exporter, otelResource)
+	if err != nil {
+		return nil
+	}
+	sfs.Add(meterProvider.Shutdown)
+	otel.SetMeterProvider(meterProvider)
+
+	loggerProvider, err := zotel.NewLoggerProvider(ctx, cfg.Log.Exporter, otelResource)
+	if err != nil {
+		return err
+	}
+	sfs.Add(loggerProvider.Shutdown)
+
+	err = setUpLogging(cfg.Log, loggerProvider)
 	if err != nil {
 		return err
 	}
@@ -460,29 +488,10 @@ func SetUpInstrumentation(
 	return nil
 }
 
-func createResource(serviceName string) (*resource.Resource, error) {
-	attributes := []attribute.KeyValue{
-		semconv.ServiceNameKey.String(serviceName),
-	}
-	if build.Version() != "" {
-		attributes = append(attributes, semconv.ServiceVersionKey.String(build.Version()))
-	}
-	return resource.Merge(
-		resource.Default(),
-		resource.NewWithAttributes("", attributes...),
-	)
-}
-
-func setUpLogging(ctx context.Context, sfs *ShutdownFuncs, cfg instrumentation.LogConfig, rsc *resource.Resource) error {
-	provider, err := otel.NewLoggerProvider(ctx, cfg.Exporter, rsc)
-	if err != nil {
-		return err
-	}
-	sfs.Add(provider.Shutdown)
-
+func setUpLogging(cfg instrumentation.LogConfig, otelProvider log.LoggerProvider) error {
 	otelHandler := otelslog.NewHandler(
 		Name,
-		otelslog.WithLoggerProvider(provider),
+		otelslog.WithLoggerProvider(otelProvider),
 	)
 
 	stdErrHandler := cfg.Format.ErrorHandler(cfg.SlogHandlerOptions())
--- internal/instrumentation/config.go
+++ internal/instrumentation/config.go
@@ -7,26 +7,28 @@ import (
 
 	slogctx "github.com/veqryn/slog-context"
 	slogotel "github.com/veqryn/slog-context/otel"
-	"github.com/zitadel/nextgen/internal/instrumentation/otel"
 	"github.com/zitadel/nextgen/internal/instrumentation/zlog"
 	"github.com/zitadel/nextgen/internal/instrumentation/zlog/extractors"
 	"github.com/zitadel/nextgen/internal/instrumentation/zlog/replacers"
+	"github.com/zitadel/nextgen/internal/instrumentation/zotel"
 	"github.com/zitadel/sloggcp"
 )
 
 type Config struct {
-	ServiceName string    `mapstructure:"service_name"`
-	Log         LogConfig `mapstructure:"log"`
+	ServiceName string       `mapstructure:"service_name"`
+	Log         LogConfig    `mapstructure:"log"`
+	Trace       TraceConfig  `mapstructure:"trace"`
+	Metric      MetricConfig `mapstructure:"metric"`
 }
 
 type LogConfig struct {
-	Level     zlog.Level          `mapstructure:"level"`
-	Streams   []zlog.Stream       `mapstructure:"streams"`
-	Mask      MaskConfig          `mapstructure:"mask"`
-	Format    LogFormat           `mapstructure:"format"`
-	AddSource bool                `mapstructure:"add_source"`
-	Errors    ErrorConfig         `mapstructure:"errors"`
-	Exporter  otel.ExporterConfig `mapstructure:"exporter"`
+	Level     zlog.Level           `mapstructure:"level"`
+	Streams   []zlog.Stream        `mapstructure:"streams"`
+	Mask      MaskConfig           `mapstructure:"mask"`
+	Format    LogFormat            `mapstructure:"format"`
+	AddSource bool                 `mapstructure:"add_source"`
+	Errors    ErrorConfig          `mapstructure:"errors"`
+	Exporter  zotel.ExporterConfig `mapstructure:"exporter"`
 }
 
 func (cfg LogConfig) SlogHandlerOptions() *slog.HandlerOptions {
@@ -126,7 +128,11 @@ type ErrorConfig struct {
 }
 
 type TraceConfig struct {
-	Fraction         float64
-	Exporter         otel.ExporterConfig
-	TrustRemoteSpans bool
+	Fraction         float64              `mapstructure:"fraction"`
+	Exporter         zotel.ExporterConfig `mapstructure:"exporter"`
+	TrustRemoteSpans bool                 `mapstructure:"trust_remote_spans"`
+}
+
+type MetricConfig struct {
+	Exporter zotel.ExporterConfig `mapstructure:"exporter"`
 }
