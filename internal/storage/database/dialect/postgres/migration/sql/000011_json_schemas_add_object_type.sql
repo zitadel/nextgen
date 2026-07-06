cde47d519b2f79d95d2998427856b3e1e2eb67aa
Wim Van Laer <wim+github@zitadel.com>
2026-07-06 10:50:08 +0200

    merge migrations

--- internal/storage/database/dialect/postgres/migration/sql/000011_json_schemas_add_object_type.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
-ALTER TABLE zitadel_nextgen.json_schemas
-ADD COLUMN object_type VARCHAR(256) COLLATE "C";
-
--- +goose Down
-ALTER TABLE zitadel_nextgen.json_schemas
-DROP COLUMN object_type;
